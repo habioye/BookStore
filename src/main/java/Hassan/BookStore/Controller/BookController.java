70055ee86b80afd9e5666b7a4248feb48cb2d7a3
habioye <78454514+habioye@users.noreply.github.com>
2024-06-11 14:42:59 -0400

    Added controller mapping for book object

---
+++ src/main/java/Hassan/BookStore/Controller/BookController.java
@@ -0,0 +1,38 @@
+package Hassan.BookStore.Controller;
+
+import Hassan.BookStore.Entity.Book;
+import Hassan.BookStore.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBook(@PathVariable Long id) {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    public Book createBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+
+
+
+}
