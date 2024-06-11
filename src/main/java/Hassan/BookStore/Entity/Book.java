d1758d55f3819e409152c7dc32c5029225224fca
habioye <78454514+habioye@users.noreply.github.com>
2024-06-11 14:42:34 -0400

    Added book entitiy

---
+++ src/main/java/Hassan/BookStore/Entity/Book.java
@@ -0,0 +1,21 @@
+package Hassan.BookStore.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+
+
+
+
+}
